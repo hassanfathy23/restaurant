de36b19c091ef20c70cf78864fb4a6b4aaa028d6
hassan <hassanfathy593@gmail.com>
2023-01-17 02:49:06 +0200

    added testimonials comp

---
+++ components/Testimonials.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Testimonials() {
+  return (
+    <div className="flex flex-row gap-20 justify-evenly items-center">
+      <div className="relative w-[610px] h-[406px]">
+        <Image src="/images/clip-waiting.png" alt="image" fill />
+      </div>
+      <div className="w-[510px] flex flex-col gap-6">
+        <p className="text-[#EC6083] font-medium text-[18px]">what they say</p>
+        <h2 className=" font-bold text-[48px] leading-none">
+          what our customer say about us
+        </h2>
+        <p className="text-black/70">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
+          varius eget pulvinar rhoncus eros, magna. Orci sed aenean scelerisque
+          arcu. Venenatis odio etiam bibendum in nulla.
+        </p>
+        <div className="flex flex-row items-center gap-2">
+          <Image src="/images/image1.png" width="80" height="80" alt="image" />
+          <div className="flex flex-col">
+            <h3 className="font-medium text-[24px]">Alex Santos</h3>
+            <p className="text-[18px] text-black/60">Customer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Testimonials from "../components/Testimonials";
 import WhatWeServe from "../components/WhatWeServe";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <WhatWeServe />
+      <Testimonials />
     </div>
   )
 }
